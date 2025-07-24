bb5d5070c43e8a6851740af46e0172be3da38ca2
Galaxy Master <Marcel.Hajek@gmx.de>
2025-07-24 14:14:35 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,39 @@
-#include <vector>
 #include <string>
+#include <variant>
+#include <vector>
+#include <span>
 
-enum class TokenKind : uint8_t {
-  OPEN_BRACKET,
-  CLOSE_BRACKET,
 
-  OPEN_BRACE,
-  CLOSE_BRACE,
 
-  EQUALS,
-
-  ASSIGN,
-
-  EndOfFile,
-};
-
-union TokenData {
-  void* nothing;
-  int integer;
-  float floating;
-  std::string string;
-};
-
-class Token {
-public:
-  Token(const TokenKind &kind, const TokenData &data)
-      : kind(kind), data(data) {}
-
-  TokenKind &getKind();
-  TokenData &getData();
-
-private:
-  TokenKind kind;
-  TokenData data;
-};
-
-TokenKind &Token::getKind() {
-  return kind;
-}
-
-TokenData &Token::getData() {
-  return data;
-}
+using Token = std::variant<
+  int8_t,
+  uint8_t,
+  int16_t,
+  uint16_t,
+  int32_t,
+  uint32_t,
+  int64_t,
+  uint64_t,
+  float,
+  double,
+  std::string,
+  std::string_view
+>;
 
 int main(int argc, const char * argv[]) {
   std::vector<Token> tokens {};
+  const std::initializer_list<uint8_t> numbers {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  const std::initializer_list<std::string_view> strings {"hello", "world"};
+
+  tokens.reserve(numbers.size());
+
+  for (const uint8_t& number : numbers) {
+    tokens.emplace_back(number);
+  }
+
+  tokens.reserve(strings.size());
+
+  for (const std::string_view& string: strings) {
+    tokens.emplace_back(string);
+  }
 }
